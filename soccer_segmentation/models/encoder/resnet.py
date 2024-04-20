b844639eebc76ccd86c7f1cf586eb689a9122ebe
otaviohrg <otaviohrguimaraes@gmail.com>
2024-04-20 09:35:35 +0200

    feat: add resnet encoders

--- soccer_segmentation/models/encoder/resnet.py
+++ soccer_segmentation/models/encoder/resnet.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
-from torchvision.models import resnet18
+from torchvision.models import resnet18, resnet34, resnet50, resnet101, resnet152
 
 
 class ResNet18(nn.Module):
@@ -18,8 +18,108 @@ class ResNet18(nn.Module):
             param.requires_grad = True
 
     def forward(self, images):
-        x0 = x = self.model.conv1(images)
-        x1 = x = self.model.layer1(self.model.maxpool(self.model.relu(self.model.bn1(x))))
+        x0 = x = self.model.relu(self.model.bn1(self.model.conv1(images)))
+        x1 = x = self.model.layer1(self.model.maxpool(x))
+        x2 = x = self.model.layer2(x)
+        x3 = x = self.model.layer3(x)
+        x4 = self.model.layer4(x)
+
+        return [x0, x1, x2, x3, x4]
+
+
+class ResNet34(nn.Module):
+    def __init__(self, train_cnn=False):
+        super(ResNet34, self).__init__()
+        self.model = resnet34(weights='IMAGENET1K_V1')
+        if not train_cnn:
+            self.freeze()
+
+    def freeze(self):
+        for param in self.model.parameters():
+            param.requires_grad = False
+
+    def unfreeze(self):
+        for param in self.model.parameters():
+            param.requires_grad = True
+
+    def forward(self, images):
+        x0 = x = self.model.relu(self.model.bn1(self.model.conv1(images)))
+        x1 = x = self.model.layer1(self.model.maxpool(x))
+        x2 = x = self.model.layer2(x)
+        x3 = x = self.model.layer3(x)
+        x4 = self.model.layer4(x)
+
+        return [x0, x1, x2, x3, x4]
+
+
+class ResNet50(nn.Module):
+    def __init__(self, train_cnn=False):
+        super(ResNet50, self).__init__()
+        self.model = resnet50(weights='IMAGENET1K_V1')
+        if not train_cnn:
+            self.freeze()
+
+    def freeze(self):
+        for param in self.model.parameters():
+            param.requires_grad = False
+
+    def unfreeze(self):
+        for param in self.model.parameters():
+            param.requires_grad = True
+
+    def forward(self, images):
+        x0 = x = self.model.relu(self.model.bn1(self.model.conv1(images)))
+        x1 = x = self.model.layer1(self.model.maxpool(x))
+        x2 = x = self.model.layer2(x)
+        x3 = x = self.model.layer3(x)
+        x4 = self.model.layer4(x)
+
+        return [x0, x1, x2, x3, x4]
+
+
+class ResNet101(nn.Module):
+    def __init__(self, train_cnn=False):
+        super(ResNet101, self).__init__()
+        self.model = resnet101(weights='IMAGENET1K_V1')
+        if not train_cnn:
+            self.freeze()
+
+    def freeze(self):
+        for param in self.model.parameters():
+            param.requires_grad = False
+
+    def unfreeze(self):
+        for param in self.model.parameters():
+            param.requires_grad = True
+
+    def forward(self, images):
+        x0 = x = self.model.relu(self.model.bn1(self.model.conv1(images)))
+        x1 = x = self.model.layer1(self.model.maxpool(x))
+        x2 = x = self.model.layer2(x)
+        x3 = x = self.model.layer3(x)
+        x4 = self.model.layer4(x)
+
+        return [x0, x1, x2, x3, x4]
+
+
+class ResNet152(nn.Module):
+    def __init__(self, train_cnn=False):
+        super(ResNet152, self).__init__()
+        self.model = resnet152(weights='IMAGENET1K_V1')
+        if not train_cnn:
+            self.freeze()
+
+    def freeze(self):
+        for param in self.model.parameters():
+            param.requires_grad = False
+
+    def unfreeze(self):
+        for param in self.model.parameters():
+            param.requires_grad = True
+
+    def forward(self, images):
+        x0 = x = self.model.relu(self.model.bn1(self.model.conv1(images)))
+        x1 = x = self.model.layer1(self.model.maxpool(x))
         x2 = x = self.model.layer2(x)
         x3 = x = self.model.layer3(x)
         x4 = self.model.layer4(x)
